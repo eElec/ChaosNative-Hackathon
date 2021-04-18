2be63d2af989c62f68867cc08cc9cd9c939ef824
Adrish Aditya <adrishaditya@gmail.com>
2021-04-19 05:24:59 +0530

    fix(server): minor changes

--- server/api/getMeetings.js
+++ server/api/getMeetings.js
@@ -7,14 +7,21 @@ router.get('/', async (req, res) => {
 	let Q_Select;
 
 	let search = searchTerm || '';
-	let from = fromDate || '1900-01-01'
-	let to = toDate || '2100-01-01'
-	Q_Select =
-		'SELECT * FROM meetings WHERE `Name` LIKE ? AND `StartTime` > ? AND `StartTime` < ?'
-	const [results, ...rest] = await connection.query(Q_Select,
-		[search + '%', from, to]
-		);
-	res.send(results);
+	let from = fromDate || '1900-01-01';
+	let to = toDate || '2100-01-01';
+	try {
+		Q_Select =
+			'SELECT * FROM meetings WHERE `Name` LIKE ? AND `StartTime` > ? AND `StartTime` < ?';
+		const [results, ...rest] = await connection.query(Q_Select, [
+			search + '%',
+			from,
+			to,
+		]);
+		return res.status(200).json(results);
+	}
+	catch(err){
+		return res.status(500).json({message: err})
+	}
 });
 
 module.exports = router;
--- server/api/removeMeeting.js
+++ server/api/removeMeeting.js
@@ -4,9 +4,15 @@ const connection = require('../config/connection');
 
 router.post('/', async (req, res) => {
 	const { ID } = req.body;
-	const Q_Delete = 'DELETE FROM meetings WHERE `ID`=?';
-	const [result, ...rest] = await connection.query(Q_Delete, [ID]);
-	res.send(result);
+	if (ID === undefined) return res.status(400).json({ message: 'Invalid ID' });
+	try {
+		const Q_Delete = 'DELETE FROM meetings WHERE `ID`=?';
+		const [result, ...rest] = await connection.query(Q_Delete, [ID]);
+	}
+	catch(err){
+		res.status(500).json({ message: err });
+	}
+	res.status(200).json({ message: 'Removed meeting' });
 });
 
 module.exports = router;
