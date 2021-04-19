4b2259c01ac2915b7deb3c08cba0928d66aae60e
Adrish Aditya <adrishaditya@gmail.com>
2021-04-19 07:06:42 +0530

    feat(client): added more tests

--- client/src/tests/testForm.test.js
+++ client/src/tests/testForm.test.js
@@ -4,8 +4,6 @@ import { render, fireEvent, act } from '@testing-library/react';
 
 import Form from '../utils/components/Form';
 
-const value = 'ABC';
-
 it('matches snapshot', () => {
 	const { asFragment } = render(<Form />);
 
@@ -49,4 +47,71 @@ describe('Form Mutate', () => {
 		rerender(<Form value={values} handleChange={mockChangeValue} />);
 		expect(getByTestId('form-test').value).toBe('CDE');
 	});
+});
+
+describe('Date And Time', () => {
+	let values = '';
+	function mutateValue(e) {
+		values = e.target.value;
+	}
+
+	it('handles valid date', () => {
+		const { getByTestId, rerender } = render(
+			<Form value={values} handleChange={mutateValue} type='date' />
+		);
+
+		act(() => {
+			fireEvent.change(getByTestId('form-test'), {
+				target: { value: '2021-04-24' },
+			});
+		});
+
+		rerender(<Form value={values} handleChange={mutateValue} />);
+		expect(getByTestId('form-test').value).toBe('2021-04-24');
+	});
+
+	it('handles invalid date', ()=>{
+		const { getByTestId, rerender } = render(
+			<Form value={values} handleChange={mutateValue} type='date' />
+		);
+
+		act(() => {
+			fireEvent.change(getByTestId('form-test'), {
+				target: { value: 'LolXD' },
+			});
+		});
+
+		rerender(<Form value={values} handleChange={mutateValue} />);
+		expect(getByTestId('form-test').value).toBe('');
+	})
+
+	it('handles valid time', () => {
+		const { getByTestId, rerender } = render(
+			<Form value={values} handleChange={mutateValue} type='time' />
+		);
+
+		act(() => {
+			fireEvent.change(getByTestId('form-test'), {
+				target: { value: '09:00' },
+			});
+		});
+
+		rerender(<Form value={values} handleChange={mutateValue} />);
+		expect(getByTestId('form-test').value).toBe('09:00');
+	});
+
+	it('handles invalid time', ()=>{
+		const { getByTestId, rerender } = render(
+			<Form value={values} handleChange={mutateValue} type='date' />
+		);
+
+		act(() => {
+			fireEvent.change(getByTestId('form-test'), {
+				target: { value: 'LolXD' },
+			});
+		});
+
+		rerender(<Form value={values} handleChange={mutateValue} />);
+		expect(getByTestId('form-test').value).toBe('');
+	})
 });
